1dec4386087eab619fbe4a4ac1a6e7c5260f96f0
JamilAbushandi <j_daotra@yahoo.com>
2022-09-01 21:38:12 -0400

    created a test

---
+++ src/practice/practice.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class practice {
+    public static void main(String[] args) {
+
+        int i = 0;
+        if (i == 0)
+            System.out.println("answer is zero");
+
+    }
+}
